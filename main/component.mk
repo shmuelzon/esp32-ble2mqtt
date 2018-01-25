609c73c5fa5e01c00feb5c1f0fb3566e7fe23c93
Assaf Inbal <shmuelzon@gmail.com>
2018-01-25 16:20:30 +0200

    Fix build-dependecies with auto-generated files

--- main/component.mk
+++ main/component.mk
@@ -3,13 +3,14 @@
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
 
-GATT_C := $(COMPONENT_PATH)/gatt.c
+GATT_INC := $(COMPONENT_PATH)/gatt.inc
 GATT_H := $(COMPONENT_PATH)/gatt.h
 
-COMPONENT_EXTRA_CLEAN := $(GATT_C) $(GATT_H)
+COMPONENT_EXTRA_CLEAN := $(GATT_INC) $(GATT_H)
 
-config.o: $(GATT_H)
+ble_utils.o: $(GATT_H)
+gatt.o: $(GATT_INC)
 
-$(GATT_C) $(GATT_H): $(PROJECT_PATH)/get_gatt_assigned_numbers.py
+$(GATT_INC) $(GATT_H): $(PROJECT_PATH)/get_gatt_assigned_numbers.py
 	$(CONFIG_PYTHON) $(PROJECT_PATH)/get_gatt_assigned_numbers.py \
-	  -H $(GATT_H) -C $(GATT_C)
+	  -H $(GATT_H) -C $(GATT_INC)
---
+++ main/gatt.c
@@ -0,0 +1,3 @@
+/* This is a dummy C file for the ESP-IDF build system to pick up and compile
+ * the auto-generated gatt.inc file into the binary */
+#include "gatt.inc"
