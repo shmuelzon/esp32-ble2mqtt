63561ba41393e1fa182c466c3320480fad7b1d06
almirus <abrarov.almir@gmail.com>
2021-11-18 23:26:38 +0300

    totally changed UI

--- www/js/ble2mqtt.js
+++ www/js/ble2mqtt.js
@@ -1,137 +1,298 @@
-/* Management */
+const RESTART = '/restart';
+const OTA_UPLOAD = '/ota/';
+const BLE_DB = '/ble/bonding_db';
+const BLE_DB_WHITELIST = '/mock/ble/bonding_db/whitelist';
+const BLE_DB_BLACKLIST = '/mock/ble/bonding_db/blacklist';
+const CONFIG_FILE_PATH = '/fs/config.json';
+const LATEST_RELEASE = 'https://github.com/shmuelzon/esp32-ble2mqtt/releases';
+const STATUS = '/status';
+
+function progress(show = true) {
+    document.getElementById('progress').style.display = show ? 'flex' : 'none';
+    // disable\enable all buttons
+    [].concat(
+        Array.from(document.getElementsByTagName('button')), //all buttons
+        Array.from(document.getElementsByClassName('button'))  //all elements same button
+    )
+        .map(item => {
+            if (show) item.setAttribute('disabled', show)
+            else item.removeAttribute('disabled')
+        });
+}
+
+function toaster(message) {
+    document.getElementById('message').style.display = 'block';
+    document.getElementById('message').innerText = message;
+    setTimeout(() => document.getElementById('message').style.display = 'none', 3000);
+}
+
 function restart() {
-  $.ajax({
-    url: '/restart',
-    type: 'POST',
-  });
+    fetch(RESTART, {
+        method: 'POST',
+    })
+        .then((res) => {
+            console.log("restart")
+            window.open("/busy.html", "_self")
+        })
+        .catch((err) => {
+            document.getElementById('message').innerText = "Can't restart";
+            console.error(err)
+        })
 }
 
-/* BLE */
-function ble_clear_bonding() {
-  $.ajax({
-    url: '/ble/bonding_db',
-    type: 'DELETE',
-  });
+function getStatus() {
+    fetch(STATUS)
+        .then(response => response.json())
+        .then(json => {
+            document.getElementById('software-version').innerHTML = json.version;
+        });
 }
 
-/* OTA */
+function getLatestReleaseInfo() {
+    fetch('https://api.github.com/repos/shmuelzon/esp32-ble2mqtt/tags')
+        .then(response => response.json())
+        .then(json => {
+            let release = json[0];
+            if (release) document.getElementById('latest-release-id').innerHTML = `(latest <a href="${LATEST_RELEASE}" target="_blank">${release.name}</a>)`;
+        });
+}
+
 function otaStartUpload(type) {
-  var file = $('#' + type + '-file')[0].files[0];
-  if (!file)
-    return;
-
-  $('[id$=upload]').attr('disabled', true);
-  $('[id$=file]').attr('disabled', true);
-  $('#ota-spinner').show();
-  $('#ota-status').text('Uploading');
-
-  $.ajax({
-    url: '/ota/' + type,
-    type: 'POST',
-    data: file,
-    processData: false,
-  })
-  .done(onOtaUploadCompleted)
-  .fail(onOtaUploadCompleted);
+    let file = document.getElementById(type + '-file').files[0];
+    if (!file)
+        return;
+    progress(true);
+    fetch(OTA_UPLOAD + type, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/octet-stream'
+        },
+        body: file
+    })
+        .then((res) => {
+            document.getElementById('message').innerText = "Upload complete";
+            window.open("/busy.html", "_self")
+        })
+        .catch((err) => {
+            toaster(`Can't upload ${type}. Please refresh page`);
+            console.error(err)
+        })
 }
 
-function onOtaUploadCompleted(data, textStatus, jqXHR) {
-  $('#ota-status').text(textStatus);
-  $('#ota-spinner').hide();
-  $('[id$=file]').attr('disabled', false);
-  $('[id$=upload]').attr('disabled', false);
+function bleClearBonding() {
+    progress(true);
+    fetch(BLE_DB, {
+        method: 'DELETE',
+    })
+        .then((res) => {
+            progress(false);
+            toaster("Cleared");
+            console.log("cleared")
+        })
+        .catch((err) => {
+            toaster("Can't clear ble");
+            console.error(err)
+        })
 }
 
-/* File Manager */
-function fileManagerGetFilesList() {
-  path = $('#file-manager-path').text();
-
-  $('#file-manager-list').empty();
-  $.ajax({
-    url: '/fs' + path,
-    type: 'GET',
-    processData: false,
-  })
-  .done(onFileManagerFilesReady);
+function downloadConfig() {
+    fetch(CONFIG_FILE_PATH, {
+        method: 'GET',
+        cache: 'no-store'
+    })
+        .then(response => response.json())
+        .then(json => {
+            document.getElementById('config-file').innerHTML = JSON.stringify(json, null, 2);
+        })
+        .catch((err) => {
+            toaster("Can't download config file");
+            console.error(err)
+        })
 }
 
-function onFileManagerFilesReady(data, textStatus, jqXHR) {
-  for (entry of data) {
-    var div = $('<div class="file-manager-entry file-manager-entry-' + entry.type + '"></div>')
-      .click(onfileManagerClick)
-      .dblclick(entry.type, onfileManagerDoubleClick);
-    div.append($('<span class="file-manager-name"></span').text(entry.name));
-    if (entry.type === 'file')
-      div.append($('<span class="file-manager-size"></span').text(entry.size));
-    $('#file-manager-list').append(div);
-  }
+function uploadConfigFile() {
+    let file = new File([document.getElementById('config-file').value], "config.json", {
+        type: "text/plain",
+    });
+    if (file.size === 0) {
+        toaster("File is empty");
+        return;
+    }
+    progress(true);
+    fetch(CONFIG_FILE_PATH, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/octet-stream'
+        },
+        body: file
+    })
+        .then(response => {
+            restart();
+        })
+        .catch((err) => {
+            toaster("Can't upload config file");
+            console.error(err)
+        })
 }
 
-function onfileManagerClick(e) {
-  $("#file-manager-list .highlight").removeClass('highlight');
-  $(this).addClass('highlight');
+//
+//API not yet implemented
+//
+function bleListUpdate() {
+    progress(true);
+    fetch(BLE_DB, {
+        method: 'GET',
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("HTTP status " + response.status);
+            }
+            return response.json();
+        })
+        .then(json => {
+            progress(false);
+            // render BLE table
+            document.getElementById('ble-list').innerHTML = json.map(item => {
+                let blackListAddDisabled = item.list === 'black' ? 'disabled' : '';
+                let blackListDeleteDisabled = item.list === 'black' ? '' : 'disabled';
+                let whiteListAddDisabled = item.list === 'white' ? 'disabled' : '';
+                let whiteListDeleteDisabled = item.list === 'white' ? '' : 'disabled';
+
+                return `
+                <tr>
+                    <td>${item.name}</td>
+                    <td>${item.mac}</td>
+                    <td><button class="small" onclick="toList('${item.mac}','','delete')">delete</button>
+                        <button class="small" onclick="toList('${item.mac}','whitelist','add')" ${whiteListAddDisabled}>add to whitelist</button>
+                        <button class="small" onclick="toList('${item.mac}','whitelist','delete')" ${whiteListDeleteDisabled}>delete from whitelist</button>
+                        <button class="small" onclick="toList('${item.mac}','blacklist','add')" ${blackListAddDisabled}>add to blacklist</button>
+                        <button class="small" onclick="toList('${item.mac}','blacklist','delete')" ${blackListDeleteDisabled}>delete from blacklist</button>
+                    </td>
+                </tr>`;
+            }).join('\n');
+            console.log("update list complete")
+        })
+        .catch((err) => {
+            toaster("Can't update list of ble");
+            console.error(err)
+        })
 }
 
-function onfileManagerDoubleClick(e) {
-  var name = $(".file-manager-name", this).text();
-
-  if (e.data == 'file')
-    fileManagerDownloadFile(name);
-  else {
-    $('#file-manager-path').append(name + '/');
-    fileManagerGetFilesList();
-  }
+//
+//API not yet implemented
+//
+function toList(mac, list, action = 'add') {
+    let list_url = list === 'whitelist' ? BLE_DB_WHITELIST :
+        (list === 'blacklist' ? BLE_DB_BLACKLIST : BLE_DB);
+    let method = action === 'add' ? 'PUT' :
+        (action === 'delete' ? 'DELETE' : 'POST')
+    fetch(list_url, {
+        method: method,
+        body: mac
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("HTTP status " + response.status);
+            }
+        })
+        .catch((err) => {
+            toaster(`Can't ${action} ble`);
+            console.error(err)
+        })
 }
 
-function fileManagerDownloadFile(fileName) {
-  window.location.href = '/fs' + $('#file-manager-path').text() + fileName;
+function loadFileManager(path) {
+    path ||= document.getElementById('file-manager-path').innerText;
+
+    fetch('/fs' + path,
+        {
+            method: "GET",
+        })
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            document.getElementById('file-manager-list').innerHTML =
+                `<tr>
+                 <td><span style="cursor: pointer; font-weight: bold" title="Up" onclick="loadFileManager('${path}')">..</span><span id="file-manager-path" style="cursor: pointer;">${path}</span></td>   
+                 <td></td>   
+                 <td></td>   
+                </tr>` +
+                data.map(entry => {
+                    let name, del, download = '';
+                    if (entry.type === 'directory') {
+                        name = `<span style="cursor: pointer" onclick="loadFileManager('${path + '/' + entry.name}')">📁 ${entry.name}</span>`;
+                    } else {
+                        name = entry.name;
+                        del = `<span class="icon-delete" title="delete file" style="cursor: pointer" onclick="deleteFile('${path + entry.name}')"></span>`;
+                        download = `<a href="/fs${path}${entry.name}" target="_blank"><span class="icon-link" title="download file"></span></a>`;
+                    }
+                    return `<tr>
+                            <td>${name}</td>
+                            <td>${entry.type === 'file' ? entry.size : ''}</td>
+                            <td>${del} 
+                                ${download}
+                            </td>
+                            </tr>`
+                }).join('\n')
+
+        })
+        .catch(err => {
+                toaster(`Can't get ${path}`);
+                console.error(err);
+            }
+        )
 }
 
-function fileManagerUp() {
-  var path = $('#file-manager-path').text().split('/');
-  path.splice(-2, 2);
-  $('#file-manager-path').text(path.join('/') + '/');
-  fileManagerGetFilesList();
+function uploadFile() {
+    let file = document.getElementById('file-manager-file').files[0];
+    if (!file) {
+        toaster("File is empty");
+        return;
+    }
+    let path = document.getElementById('file-manager-path').innerText;
+    progress(true);
+    fetch('/fs' + path + file.name, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/octet-stream'
+        },
+        body: file
+    })
+        .then(response => {
+            toaster("OK");
+            progress(false);
+            loadFileManager(path);
+        })
+        .catch((err) => {
+            toaster("Can't upload file");
+            console.error(err)
+        })
 }
 
-function fileManagerDelete() {
-  var name = $(".file-manager-entry.highlight .file-manager-name").text();
-  if (name == '')
-    return;
-
-  $.ajax({
-    url: '/fs' + $('#file-manager-path').text() + name,
-    type: 'DELETE',
-    processData: false,
-  })
-  .done(fileManagerGetFilesList);
+function deleteFile(fullName) {
+    if (!fullName) {
+        return;
+    }
+    fetch('/fs' + fullName, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/octet-stream'
+        },
+    })
+        .then(response => {
+            toaster("OK");
+            progress(false);
+            loadFileManager();
+        })
+        .catch((err) => {
+            toaster("Can't delete file");
+            console.error(err)
+        })
 }
 
-function startFileManagerUpload() {
-  var file = $('#file-manager-file')[0].files[0];
-  if (!file)
-    return;
-
-  $('[id$=upload]').attr('disabled', true);
-  $('[id$=file]').attr('disabled', true);
-
-  $.ajax({
-    url: '/fs' + $('#file-manager-path').text() + file.name,
-    type: 'POST',
-    data: file,
-    processData: false,
-  })
-  .done(onFileManagerUploadCompleted)
-  .fail(onFileManagerUploadCompleted);
-}
-
-function onFileManagerUploadCompleted(data, textStatus, jqXHR) {
-  $('[id$=file]').attr('disabled', false);
-  $('[id$=upload]').attr('disabled', false);
-  fileManagerGetFilesList();
-}
-
-/* Generic */
-$(document).ready(function() {
-  fileManagerGetFilesList();
+document.addEventListener("DOMContentLoaded", event => {
+    getStatus();            // device status
+    getLatestReleaseInfo(); // get github version
+    bleListUpdate()         // update ble table
 });
